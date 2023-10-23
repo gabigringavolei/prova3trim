d240b29277b53a4ae99b4983f6ac9aa47982032a
gabigringavolei <130366455+gabigringavolei@users.noreply.github.com>
2023-10-23 11:27:21 -0300

    Add files via upload

---
+++ frontEnd3trimLeoncio-main/calculo.js
@@ -0,0 +1,14 @@
+function Calcular(){
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+    let cachorrinho = document.querySelector("#primeiro-cachorrinho");
+    let tdHumano = cachorrinho.querySelector(".info-Humano");
+    
+    let Humano = tdHumano.textContent;
+    letimc = Anocachorrinho/(Humano * 7)
+    
+    console.log(idadeCanina);
+
+    //BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    //POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING
+}
